4be59902f8bded8e59b6fd7648b04a309ff7cb8f
Nechama Haschel <chumie@Live.com>
2024-08-14 08:54:50 -0400

    updated

--- books.py
+++ books.py
@@ -21,15 +21,29 @@ def load_from_files():
         open(booksFile,'w')
     else:
         with open(booksFile, 'r') as file:
-            data = json.load(file)
-            books.update(data)
+            file_contents = file.read()
+            if file_contents.strip():  # Check if the file content is not empty
+                try:
+                    data = json.loads(file_contents)
+                    books.update(data)
+                except json.JSONDecodeError:
+                    print("Error: Invalid JSON format in the file.")
+            else:
+                print("File is empty.")
             
     if not os.path.exists(ebooksFile):
         open(ebooksFile,'w')
     else:
         with open(ebooksFile, 'r') as file:
-            data = json.load(file)
-            ebooks.update(data)
+            file_contents = file.read()
+            if file_contents.strip():  # Check if the file content is not empty
+                try:
+                    data = json.loads(file_contents)
+                    ebooks.update(data)
+                except json.JSONDecodeError:
+                    print("Error: Invalid JSON format in the file.")
+            else:
+                print("File is empty.")
             
 class Library:
     def __init__(self, name):
@@ -63,7 +77,7 @@ class Library:
                 try:
                     copies = int(copies)
                     break
-                except: "You didn't enter a number.  Try again"
+                except: print("You didn't enter a number.  Try again")
             
             if copies>0:
                 thisbook["copies"]=thisbook["copies"]+copies
@@ -83,7 +97,7 @@ class Library:
         ISBN = input("Enter ISBN: ")
         if ISBN =="":
             print("Must enter an ISBN to add a book. Cancelling...")
-            return;
+            return
         isfound = Library.searchBook(ISBN)
         if isfound == 1:
             return
@@ -97,22 +111,42 @@ class Library:
             except ValueError:
                 print("You didn't enter a number.  Try again")
             
-        new_book = Book(title, author, ISBN, copies)
-        books[ISBN] = {
-        "title": new_book.title,
-        "author": new_book.author,
-        "ISBN": new_book.ISBN,
-        "copies": new_book.copies
-        }
-
-        books[ISBN] = new_book
-
-#        new_book = {"title": title, "author": author, "ISBN": ISBN, "copies":int(copies)}
-#        books[ISBN] = new_book  
+        new_book = {"title": title, "author": author, "ISBN": ISBN, "copies":int(copies)}
+        books[ISBN] = new_book  
                 
-    def remove_member():
-        pass
-    
+    def remove_member(member):
+        membername = member['name']
+        members.remove(member)    
+        input(f"{membername} as been removed.  Press enter to continue")
+
+
+    def remove_book():
+        ISBN = input("Enter ISBN: ")
+        if ISBN =="":
+            print("Must enter an ISBN to add a book. Cancelling...")
+            return
+        if ISBN in books:
+            book = books[ISBN] 
+            while True:
+                cntBooksToRemove = input(f"There are {book['copies']} copies of this book.  How many do you want to remove?")
+                try:
+                    cntBooksToRemove = int(cntBooksToRemove)
+                    break
+                except:
+                    print("You must enter a number.")
+            if cntBooksToRemove==0:
+                input("You entered 0.  Nothing will be removed.  Press enter to continue.")
+                return 
+            if cntBooksToRemove>book['copies']:
+                input("You cannot remove more books from this ISBN than the library has. Press enter to continue.")
+                return
+            if cntBooksToRemove==book['copies']:
+                books.pop(ISBN, None)
+                input(f"The book with ISBN# {ISBN} has been removed from the library.  Press enter to continue.")
+            if cntBooksToRemove<book['copies']:
+                book['copies'] = book['copies'] - cntBooksToRemove
+                input(f"There are now {book['copies']} left in the library for {book['ISBN']}.  Press enter to continue.")
+        
     def savefile():
         with open(membersFile,'w') as file:
             for member in members:
@@ -122,22 +156,21 @@ class Library:
         with open(booksFile,'w') as file:
             json.dump(books, file, indent=4)
                 
+        with open(ebooksFile,'w') as file:
+            json.dump(ebooks, file, indent=4)
                 
-    def displayState():
-        pass   
-    
     def display_library_data():
         while True:
             listChoice = input("""
                 1. Display Members
                 2. Display Books
-                X. Return to previous menu""").upper()
+                3. Display eBooks
+                X. Return to previous menu """).upper()
 
             if listChoice == "1":
                 for member in members:
                     print(f"""
                     ID: \033[1m{member['id']:5}\033[0m | Name: \033[1m{member['name']:30}\033[0m""", end='')
-                    break
                 print("\n")
             if not members:
                 print("There are no members yet.")   
@@ -147,6 +180,13 @@ class Library:
                     print(f"""
                         ISBN: \033[1m{isbn:<6}\033[0m  |  Title: \033[1m{book_info['title']:<20}\033[0m  | Author: \033[1m{book_info['author']:<20}\033[0m  |  Copies: \033[1m{book_info['copies']}\033[0m""", end='')                   
                 print("\n")
+                
+            if listChoice == "3":
+                for id, ebook_info in ebooks.items():
+                    print(f"""
+                        ISBN:   Title: \033[1m{ebook_info['title']:<20}\033[0m  | Author: \033[1m{ebook_info['author']:<20}\033[0m  |  \033[1m{ebook_info['ISBN']:<6}\033[0m  | Format: \033[1m{ebook_info['format']}\033[0m""", end='')                   
+                print("\n")
+
                     
             if listChoice=="X":
                 return
@@ -193,6 +233,7 @@ class Member(Library):
                     book['copies'] =- 1
                 else:
                     input(f"{self['title']} is borrowed by another member. A Copy is not available. Press enter to continue.")
+
     def return_book(self):
         print(f"""
                     Welcome {self['name']}.
@@ -228,50 +269,44 @@ class Member(Library):
                 if ifsure=="Y":
                     members.remove(member)
 
-class EBook(Book):
-    def __init__(self, title, author, ISBN, copies, file_format):
-        super().__init__(title, author, ISBN, copies)
+class eBook(Book):
+    def __init__(self, id, title, author, ISBN, file_format):
+        super().__init__(id, title, author, ISBN)
         self.file_format = file_format
 
     def __str__(self):
         return f"{super().__str__()}, File Format: {self.file_format}"
 
     def __repr__(self):
-        return f"EBook({self.title}, {self.author}, {self.ISBN}, {self.copies}, {self.file_format})"
+        return f"eBook({self.title}, {self.author}, {self.ISBN}, {self.copies}, {self.file_format})"
 
-    def add_pdf_book():
+    def add_ebook():
         title = input("Enter EBook Title: ")
         author = input("Enter name of author: ")
         ISBN = input("Enter ISBN: ")
-        while True:
-            copies = input('How many copies? ')
-            try:
-                copies = int(copies)
-                break
-            except ValueError:
-                print("You didn't enter a number. Try again")
+        file_format = input("You can enter 1 - PDF  2 - EPUB  3 - txt     4 - Doc   5 - xls.  Enter file type (number listed or other format.):")
         
-        new_pdf_book = EBook(title, author, ISBN, copies, "PDF")
-        books[ISBN] = new_pdf_book
-        print(f"PDF EBook '{title}' added with ISBN: {ISBN}")
+        ebook_id = eBook.get_next_ebook_id()
+        formats = {
+        "1": "PDF",
+        "2": "EPUB",
+        "3": "txt",
+        "4": "Doc",
+        "5": "xls"
+        }
+        file_format = formats.get(file_format, "Other format")
+                
+        new_ebook = {"title": title, "author": author, "ISBN": ISBN, "format": file_format}
+        ebooks[ebook_id] = new_ebook 
+        print(f"PDF eBook '{title}' added with ISBN: {ISBN}")
+        print(ebooks)
 
-    def add_epub_book():
-        title = input("Enter EBook Title: ")
-        author = input("Enter name of author: ")
-        ISBN = input("Enter ISBN: ")
-        while True:
-            copies = input('How many copies? ')
-            try:
-                copies = int(copies)
-                break
-            except ValueError:
-                print("You didn't enter a number. Try again")
-        
-        new_epub_book = EBook(title, author, ISBN, copies, "EPUB")
-        books[ISBN] = new_epub_book
-        print(f"EPUB EBook '{title}' added with ISBN: {ISBN}")
-    
-                    
+    def get_next_ebook_id():
+        if not ebooks:
+            return "1"
+        max_id = int(max(ebooks.keys()))
+        return str(max_id + 1)
+                   
 load_from_files()
 
 choice=" "
@@ -279,10 +314,10 @@ choice=" "
 while choice != "X":
     print("\nLibrary Management System:")
     print("1. Member sign-in")
-    print("2. Add Members")
-    print("3. Add Books")
-    print("4. Remove Books")
-    print("5. Add Ebooks")
+    print("2. Add Member")
+    print("3. Add Book")
+    print("4. Remove Book")
+    print("5. Add eBook")
     print("6. Display current state of the library and members")
     print("X. Exit")
 
@@ -308,6 +343,7 @@ while choice != "X":
                     Member.return_book(member);
                 if memberTask =="3":
                     Member.remove_member(member)
+                    break
                 if memberTask =="X":
                     break
             
@@ -317,14 +353,9 @@ while choice != "X":
     elif choice == "3":
         Library.add_book()
     elif choice == "4":
-        pass
+        Library.remove_book()
     elif choice == "5":
-        while True:
-            ebookChoice = input("""
-                Ebook Menu
-                1. Add PDF
-                2. Add 
-                                """) 
+        eBook.add_ebook()
     elif choice == "6":
         Library.display_library_data()
     elif choice=="X":
